13916837a7227a471b2fb6ca56f5941dca2c17ff
SlowBro904 <CBdeVidal.jk1@Gmail.com>
2018-04-27 18:03:01 -0400

    Added MySQL auth example, fixed Redis example
    Created a MySQL auth example that matches the Redis example in functionality. Added logging to the Redis. Fixed an issue in the Redis where adding a user on the command line gave an error importing mosquitto_auth.

--- examples/redis_auth.py
+++ examples/redis_auth.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python
 # Example Redis authentication
+# 
+# To use, add this to mosquitto.conf:
+# 
+#   auth_plugin /path/to/auth_plugin_pyauth.so
+#   auth_opt_pyauth_module redis_auth
+# 
+# Example:
+# 
+#   auth_plugin /usr/mosquitto_pyauth/auth_plugin_pyauth.so
+#   auth_opt_pyauth_module redis_auth
 #
+# Then start the daemon with the PYTHONPATH variable, for example:
+# 
+#   export PYTHONPATH=/path/to/this/file/; mosquitto -c /path/to/mosquitto.conf
+# 
 # Run from command line to set password and acl, for example:
 #
 #   python redis_auth.py foo foobar '/foo/#'
@@ -10,44 +25,53 @@
 
 import hashlib
 import redis
-import mosquitto_auth
+import encodings.idna
 
 redis_conn = None
 
 def plugin_init(opts):
+    # Import this inside every module that logs because we cannot import it if __name__ == '__main__'
+    import mosquitto_auth
+    
     global redis_conn
     conf = dict(opts)
     redis_host = conf.get('redis_host', '127.0.0.1')
     redis_port = conf.get('redis_port', 6379)
     redis_conn = redis.StrictRedis(redis_host, redis_port)
-    print('redis initialized', redis_host, redis_port)
+    mosquitto_auth.log(mosquitto_auth.LOG_DEBUG, 'redis initialized %s %s' % (redis_host, redis_port,))
 
 def unpwd_check(username, password):
+    import mosquitto_auth
+    
     val = redis_conn.hget('mosq.' + username, 'auth')
     if not val:
-        print('AUTH: no such user:', username)
+        mosquitto_auth.log(mosquitto_auth.LOG_DEBUG, 'AUTH: no such user: %s' % username)
         return False
     salt, hashed = val.split(b':')
     check = hashlib.sha1(salt + password.encode()).hexdigest().encode()
     ok = (check == hashed)
-    print('AUTH: user=%s, password matches = %s' % (username, ok))
+    mosquitto_auth.log(mosquitto_auth.LOG_DEBUG, 'AUTH: user=%s, password matches=%s' % (username, ok,))
     return ok
 
 def acl_check(clientid, username, topic, access):
+    import mosquitto_auth
+    
     if username is None:
-        print('AUTH required')
+        mosquitto_auth.log(mosquitto_auth.LOG_DEBUG, 'AUTH required')
         return False
     pat = redis_conn.hget('mosq.' + username, 'acl')
     if not pat:
-        print('ACL: no such user:', username)
-        return False
+        mosquitto_auth.log(mosquitto_auth.LOG_DEBUG, 'ACL: no such user: %s' % username)
+        return True
     matches = mosquitto_auth.topic_matches_sub(pat.decode(), topic)
-    print('ACL: user=%s topic=%s, matches = %s' % (username, topic, matches))
+    mosquitto_auth.log(mosquitto_auth.LOG_DEBUG, 'ACL: user=%s topic=%s, pat=%s, matches=%s' % (username, topic, pat, matches,))
     return matches
 
 
 def psk_key_get(identity, hint):
-    print('psk_key_get', identity, hint)
+    import mosquitto_auth
+    
+    mosquitto_auth.log(mosquitto_auth.LOG_DEBUG, 'psk_key_get %s %s' % (identity, hint,))
     return '0123456789'
 
 
